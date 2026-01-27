1df228528560ec749786e94c6e5b83c01bf885e6
Samuel <samuel.silveira@sp.senai.br>
2026-01-27 14:06:44 -0300

    login e protecao da rota de cadastro/edicao + instalacao js-cookie

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Produtos from "./pages/Produtos/Produtos"
 import Home from "./pages/Home/Home"
 import Cadastro from "./pages/Cadastro/Cadastro"
+import RotaProtegida from "./components/RotaProtegida/RotaProtegida"
+import Login from "./pages/Login/Login"
 
 
 function App() {
@@ -10,10 +12,15 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/produtos/:categoria" element={ <Produtos /> } />
-          <Route path="/produtos/pesquisa" element={ <Produtos /> } />
-          <Route path="/produtos/cadastro" element={ <Cadastro /> } />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/produtos/:categoria" element={<Produtos />} />
+          <Route path="/produtos/pesquisa" element={<Produtos />} />
+          <Route path="/produtos/cadastro" element={
+            <RotaProtegida>
+              <Cadastro />
+            </RotaProtegida>
+          } />
         </Routes>
       </BrowserRouter>
     </>
