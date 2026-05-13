54cc1bde214af6c5bd821835666d958368b67faf
Samuel <samuel.silveira@sp.senai.br>
2026-05-13 11:37:34 -0300

    add interceptor com resgate do cookie para autenticar as requisições feitas na API com rotas protegidas

--- src/services/bolosService.tsx
+++ src/services/bolosService.tsx
@@ -3,57 +3,91 @@ import type { Bolo } from "../types/Bolo";
 
 const API_BASE = "http://localhost:5103/api";
 
+// Função para pegar cookie pelo nome
+const getCookie = (nome: string): string | null => {
+  const valor = `; ${document.cookie}`;
+  const partes = valor.split(`; ${nome}=`);
+
+  if (partes.length === 2) {
+    return partes.pop()?.split(";").shift() ?? null;
+  }
+
+  return null;
+};
+
+const api = axios.create({
+  baseURL: API_BASE,
+  withCredentials: true,
+});
+
+// Interceptor para adicionar o token automaticamente
+api.interceptors.request.use((config) => {
+  const token = getCookie("auth_token");
+
+  console.log("TOKEN:", token);
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export const getBolos = async (): Promise<Bolo[]> => {
   try {
-    const resposta = await axios.get(`${API_BASE}/bolos`);
+    const resposta = await api.get("/bolos");
     return resposta.data;
   } catch (error) {
     console.error("Erro ao buscar os dados: ", error);
     throw error;
   }
-}
+};
 
 export const deleteBolo = async (idBolo: string): Promise<void> => {
   try {
-    await axios.delete(`${API_BASE}/bolos/${idBolo}`);
+    await api.delete(`/bolos/${idBolo}`);
   } catch (error) {
     console.error("Erro ao deletar o bolo: ", error);
     throw error;
   }
-}
+};
 
-export const enviarFotoParaAPI = async (file: File): Promise<string | undefined> => {
+export const enviarFotoParaAPI = async (
+  file: File
+): Promise<string | undefined> => {
   const formData = new FormData();
   formData.append("file", file);
 
   try {
-    const res = await axios.post(`${API_BASE}/upload`, formData, {
-      headers: { "Content-Type": "multipart/form-data" }
+    const res = await api.post("/upload", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
     });
+
     return res.data.caminhoImagem ?? res.data.CaminhoImagem;
   } catch (error) {
     console.error("Erro no upload da imagem: ", error);
     return undefined;
   }
-}
+};
 
 export const postBolo = async (bolo: Bolo): Promise<void> => {
   try {
-    await axios.post(`${API_BASE}/bolos`, bolo);
+    await api.post("/bolos", bolo);
   } catch (error) {
     console.error("Erro ao cadastrar o bolo", error);
     throw error;
   }
-}
+};
 
 export const putBolo = async (bolo: Bolo): Promise<void> => {
   try {
     if (!bolo.id) {
       throw new Error("ID do bolo não informado");
     }
-    await axios.put(`${API_BASE}/bolos/${bolo.id}`, bolo);
+
+    await api.put(`/bolos/${bolo.id}`, bolo);
   } catch (error) {
     console.error("Erro ao atualizar o bolo", error);
     throw error;
   }
-}
+};
